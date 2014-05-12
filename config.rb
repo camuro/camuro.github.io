ce3c81baa2c4b2425c10c0c861c4baf1fbba83f1
kumano ayumi <kumayuzuki@gmail.com>
2014-05-12 22:25:18 +0900

    Add works portfolio

--- config.rb
+++ config.rb
@@ -27,8 +27,9 @@ end
 
 page "/feed.xml", :layout => false
 
+page "/works/*", :layout => "work"
 
-### 
+###
 # Compass
 ###
 
@@ -46,13 +47,13 @@ page "/feed.xml", :layout => false
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -87,21 +88,21 @@ configure :build do
 
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
---
+++ source/works/oshinagaki.markdown
@@ -0,0 +1,8 @@
+---
+layout: work
+title: おしながき
+published: true
+date: 2012-04-11 13:17
+tags: []
+---
+test
