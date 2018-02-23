7027699ecceb232fae4fd98089b2969becf8db29
kumano ayumi <kumayuzuki@gmail.com>
2018-02-23 10:13:42 +0900

    手元で復元

--- build/javascripts/mylibs/google.js
+++
@@ -1,1 +0,0 @@
-var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-36103295-1"]),_gaq.push(["_trackPageview"]),function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();
